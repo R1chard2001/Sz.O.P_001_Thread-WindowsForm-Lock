e3bc811266086f9fa0931f8cdddb7533d8646109
Matuch Richárd <matuchrichard@citromail.hu>
2022-09-25 19:04:22 +0200

    A projekt és dokumentum feltöltése

---
+++ src/01_Sz.O.P/01_Sz.O.P/Form1.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace _01_Sz.O.P
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Canvas = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.Canvas)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Canvas
+            // 
+            this.Canvas.BackColor = System.Drawing.Color.White;
+            this.Canvas.Location = new System.Drawing.Point(13, 13);
+            this.Canvas.Name = "Canvas";
+            this.Canvas.Size = new System.Drawing.Size(857, 428);
+            this.Canvas.TabIndex = 0;
+            this.Canvas.TabStop = false;
+            this.Canvas.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Canvas_MouseDown);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(882, 453);
+            this.Controls.Add(this.Canvas);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.Canvas)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox Canvas;
+    }
+}
+
---
+++ src/01_Sz.O.P/01_Sz.O.P/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace _01_Sz.O.P
+{
+    public partial class Form1 : Form
+    {
+        Graphics g; // osztályban lévő változó, külön metódusokban elérhessünk,
+                    // majd ez segítségével rajzolunk
+
+        Random rnd = new Random(); // csak az DrawRandomLine érdekesebbé tétele miatt
+        public Form1()
+        {
+            InitializeComponent();
+            g = Canvas.CreateGraphics(); // megmondjuk, hogy ennek használjuk
+        }
+
+        public void DrawRandomLine()
+        {
+            int x1 = rnd.Next(Canvas.Width);
+            int y1 = rnd.Next(Canvas.Height);
+            int x2 = rnd.Next(Canvas.Width);
+            int y2 = rnd.Next(Canvas.Height);
+
+            g.DrawLine(Pens.Black, x1,y1,x2,y2);
+        }
+
+        // kattintáskor véletlen helyre rak egy vonalat
+        //private void Canvas_MouseDown(object sender, MouseEventArgs e)
+        //{ 
+        //    Thread t = new Thread(DrawRandomLine); 
+        //    t.Start();
+        //}
+
+        // helytelen lock-olás, a metódus teljes futása alatt ellehetetlenítjük
+        // a megfelelő erőforráshoz a hozzáférést
+        //private void DrawLine(int x, int y)
+        //{
+        //    lock (g)
+        //    {
+        //        // A Canvas közepétől rajzolja a vonalat
+        //        g.DrawLine(Pens.Black, Canvas.Width / 2, Canvas.Height / 2, x, y);
+
+        //        // a megadott ideig (ms) vár
+        //        Thread.Sleep(2500);
+
+        //        // "Törli" a vonalat
+        //        g.DrawLine(Pens.White, Canvas.Width / 2, Canvas.Height / 2, x, y);
+        //    }
+
+        //}
+
+        // Helyes lock-olás, csak a kritikus szakaszban foglaljuk le
+        // a kívánt erőforrást
+        private void DrawLine(int x, int y)
+        {
+            lock (g) // A Canvas közepétől rajzolja a vonalat
+            {
+                g.DrawLine(Pens.Black, Canvas.Width / 2, Canvas.Height / 2, x, y);
+            }
+            // a megadott ideig (ms) vár
+            Thread.Sleep(2500);
+            lock (g) // "Törli" a vonalat
+            {
+                g.DrawLine(Pens.White, Canvas.Width / 2, Canvas.Height / 2, x, y);
+            }
+        }
+
+        private void Canvas_MouseDown(object sender, MouseEventArgs e)
+        {
+            Thread t = new Thread(() => DrawLine(e.X, e.Y));
+            t.Start();
+        }
+    }
+}
